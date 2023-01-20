3321f68edc567fcd00d3b5b754af95aaef981f7f
supparatleelee <supparat.lst@gmail.com>
2023-01-20 15:01:17 +0700

    todo controller testing: res.status 201

--- __test__/unit/todo.controller.spec.js
+++ __test__/unit/todo.controller.spec.js
@@ -3,6 +3,13 @@ const { createTodo } = require('../../src/controller/todo.controller');
 const { Todo } = require('../../src/model');
 const todo = require('../mocks/todo.json');
 
+let req, res, next;
+beforeAll(() => {
+  // Arrange Global
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+});
+
 describe('Todo controller : Create todo', () => {
   it('should have todo.controller.createTodo as a function', () => {
     expect(typeof createTodo).toBe('function');
@@ -10,8 +17,8 @@ describe('Todo controller : Create todo', () => {
 
   it('should call TodoModel.create', () => {
     // Arrange
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
+    // let req = httpMocks.createRequest();
+    // let res = httpMocks.createResponse();
     // Setup expectation
     Todo.create = jest.fn();
 
@@ -23,8 +30,8 @@ describe('Todo controller : Create todo', () => {
   });
 
   it('should call Todo.create with newTodo', () => {
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
+    // let req = httpMocks.createRequest();
+    // let res = httpMocks.createResponse();
     req.body = todo;
 
     Todo.create = jest.fn();
@@ -32,5 +39,15 @@ describe('Todo controller : Create todo', () => {
     createTodo(req, res);
 
     expect(Todo.create).toHaveBeenCalledWith(todo);
+  });
+
+  it('should return status 201 ub response', () => {
+    req.body = todo;
+
+    // Act
+    createTodo(req, res);
+
+    // Assert
+    expect(res.statusCode).toBe(201);
   });
 });
--- src/controller/todo.controller.js
+++ src/controller/todo.controller.js
@@ -3,4 +3,5 @@ const { Todo } = require('../model');
 exports.createTodo = (req, res) => {
   const todo = req.body;
   Todo.create(todo);
+  res.status(201).send();
 };
